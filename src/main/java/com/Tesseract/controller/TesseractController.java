c93ca46f23b4827ffd4df7b4975589055d78429f
espkishore <espkishore@gmail.com>
2020-07-31 13:15:09 +0530

    first commit

---
+++ src/main/java/com/Tesseract/controller/TesseractController.java
@@ -0,0 +1,34 @@
+package com.Tesseract.controller;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import com.Tesseract.service.ITesseractService;
+
+
+
+@RestController
+public class TesseractController {
+	
+	@Autowired
+	ITesseractService tesseractService;
+	
+
+
+	@PostMapping("/ocr")
+	public ResponseEntity<String> convertToText(@RequestParam(name="file") MultipartFile file) throws Exception{
+		return tesseractService.convertToText(file);
+		
+	}
+	
+	
+
+}
